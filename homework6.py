cf8cb396b4b323dc41c64b96ab177d836999c5bd
VeraBelugina <vryshanchik-cherry@mail.ru>
2024-07-24 00:42:51 +0300

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,14 @@
+my_dict = {'Гога': 1990, 'Гоша': 2006, 'Юрий': 1999}
+print(my_dict)
+print(my_dict['Гога'])
+print(my_dict.get('Катерина'))
+my_dict.update({'Гора': 1962, 'Жора': 1977})
+print(my_dict)
+del my_dict['Юрий']
+print(my_dict)
+my_set = {9, 3, 5, 3, 'pop', 3.5, False, 'pop'}
+print(my_set)
+my_set.update({505, 'lol'})
+print(my_set)
+my_set.remove(3)
+print(my_set)
